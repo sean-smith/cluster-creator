54d939df44243b1ce21f014fc0bab62856c8dde3
Sean Smith <seaam@amazon.com>
2021-10-13 10:08:46 -0600

    Added templates
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- assets/js/create.js
+++ assets/js/create.js
@@ -43,7 +43,7 @@ $(function() {
   // Deploy API
   $("#api").click(function() {
     region = $("#region").val();
-    link = `https://${region}.console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/create/review?stackName=pcluster-api&templateURL=https://cluster-creator.s3.amazonaws.com/parallelcluster-api.yaml&param_EnableIamAdminAccess=true&param_CreateApiUserRole=false`;
+    link = `https://${region}.console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/create/review?stackName=pcluster-api&templateURL=https://cluster-creator.s3.amazonaws.com/assets/cfn/parallelcluster-api.yaml&param_EnableIamAdminAccess=true&param_CreateApiUserRole=false`;
     window.open(link, '_blank');
   });
 
@@ -53,7 +53,7 @@ $(function() {
     config_base64 = btoa(config_yml);
     cluster_name = $("#cluster_name").val();
     region = $("#region").val();
-    link = `https://${region}.console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/create/review?stackName=cluster-creator-${cluster_name}&templateURL=https://cluster-creator.s3.amazonaws.com/template.yml&param_APIStack=pcluster-api&param_ClusterName=${cluster_name}&param_ConfigFile=${config_base64}`;
+    link = `https://${region}.console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/create/review?stackName=cluster-creator-${cluster_name}&templateURL=https://cluster-creator.s3.amazonaws.com/assets/cfn/template.yml&param_APIStack=pcluster-api&param_ClusterName=${cluster_name}&param_ConfigFile=${config_base64}`;
     window.open(link, '_blank');
   });
 
